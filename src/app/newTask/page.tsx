ac31b65405ecf48b577e06c7bcd004a825fa04de
SamuDev <samudev01@gmail.com>
2025-01-28 17:37:19 -0300

    sections pages

---
+++ src/app/newTask/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TaskForm } from './task-form'
+
+function NewPage() {
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <TaskForm />
+        </div>
+    )
+}
+
+export default NewPage
---
+++ src/app/newTask/task-form.tsx
@@ -0,0 +1,71 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+
+export function TaskForm() {
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Crear tarea</CardTitle>
+                <CardDescription>Llena el formulario debajo para crear una nueva tarea</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Nombre</Label>
+                            <Input
+                                id="name"
+                                placeholder="Nombre de tu tarea"
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="description">Descripción</Label>
+                            <Textarea
+                                name="description"
+                                id="description"
+                                placeholder="Descipción de la tarea..."
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="priority">Prioridad</Label>
+                            <Select>
+                                <SelectTrigger id="priority">
+                                    <SelectValue placeholder="Seleccionar" />
+                                </SelectTrigger>
+                                <SelectContent position="popper">
+                                    <SelectItem value="low">Baja</SelectItem>
+                                    <SelectItem value="medium">Media</SelectItem>
+                                    <SelectItem value="high">Alta</SelectItem>
+                                    <SelectItem value="urgent">Urgente</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button variant="outline">Cancel</Button>
+                <Button>Deploy</Button>
+            </CardFooter>
+        </Card>
+    )
+}
