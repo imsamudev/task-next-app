1e03b24380637d58b494d37a6d155dccbca6b128
SamuDev <samudev01@gmail.com>
2025-01-28 18:01:17 -0300

    header

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ThemeToggle } from '../theme-toggle-btn'
+import Link from 'next/link'
+import { buttonVariants } from './button'
+
+const Header = () => {
+    return (
+        <header className='flex justify-between p-1 py-2'>
+            <h1 className='text-3xl'> MyTasky </h1>
+            <div className='flex gap-x-2 items-start'>
+                <Link href='/newTask'
+                    className={buttonVariants({ variant: 'secondary' })}
+                >
+                    Crear Tarea
+                </Link>
+                <ThemeToggle />
+            </div>
+        </header>
+    )
+}
+
+export default Header
