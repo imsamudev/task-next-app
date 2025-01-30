a54c44d720963f49a4130da6c5a062c0b9cd4edf
SamuDev <samudev01@gmail.com>
2025-01-30 19:41:03 -0300

    update taskform

--- src/app/newTask/task-form.tsx
+++ src/app/newTask/task-form.tsx
@@ -19,20 +19,48 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
+import prisma from "@/lib/prisma"
+import { redirect } from "next/navigation"
 
 export function TaskForm() {
+
+    async function createTask(formData: FormData) {
+        "use server"
+        const name = formData.get("name")?.toString()
+        const description = formData.get("description")?.toString()
+        const priority = formData.get("priority")?.toString()
+
+        console.log({ name, description, priority });
+
+        if (!name || !description || !priority) {
+            return;
+        }
+
+        const newTask = await prisma.task.create({
+            data: {
+                name: name,
+                description: description,
+                priority: priority
+            }
+        })
+
+        console.log(newTask);
+        redirect('/')
+    }
+
     return (
-        <Card className="w-[350px]">
-            <CardHeader>
-                <CardTitle>Crear tarea</CardTitle>
-                <CardDescription>Llena el formulario debajo para crear una nueva tarea</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
+        <form action={createTask}>
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Crear tarea</CardTitle>
+                    <CardDescription>Llena el formulario debajo para crear una nueva tarea</CardDescription>
+                </CardHeader>
+                <CardContent>
                     <div className="grid w-full items-center gap-4">
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">Nombre</Label>
                             <Input
+                                name="name"
                                 id="name"
                                 placeholder="Nombre de tu tarea"
                             />
@@ -42,30 +70,32 @@ export function TaskForm() {
                             <Textarea
                                 name="description"
                                 id="description"
-                                placeholder="Descipción de la tarea..."
+                                placeholder="Descripción de la tarea..."
                             />
                         </div>
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="priority">Prioridad</Label>
-                            <Select>
+                            <Select
+                                name="priority">
                                 <SelectTrigger id="priority">
                                     <SelectValue placeholder="Seleccionar" />
                                 </SelectTrigger>
                                 <SelectContent position="popper">
-                                    <SelectItem value="low">Baja</SelectItem>
-                                    <SelectItem value="medium">Media</SelectItem>
-                                    <SelectItem value="high">Alta</SelectItem>
-                                    <SelectItem value="urgent">Urgente</SelectItem>
+                                    <SelectItem value="Baja">Baja</SelectItem>
+                                    <SelectItem value="Media">Media</SelectItem>
+                                    <SelectItem value="Alta">Alta</SelectItem>
+                                    <SelectItem value="Urgente">Urgente</SelectItem>
                                 </SelectContent>
                             </Select>
                         </div>
                     </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-                <Button variant="outline">Cancel</Button>
-                <Button>Deploy</Button>
-            </CardFooter>
-        </Card>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button variant="outline">Cancel</Button>
+                    <Button
+                        type="submit">Listo</Button>
+                </CardFooter>
+            </Card>
+        </form>
     )
 }
