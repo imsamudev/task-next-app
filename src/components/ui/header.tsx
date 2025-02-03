377bcb0c43174701e3acad738ee33e715c3a5ea5
SamuDev <samudev01@gmail.com>
2025-02-03 15:20:36 -0300

    update links

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -6,11 +6,14 @@ import { buttonVariants } from './button'
 const Header = () => {
     return (
         <header className='flex justify-between p-1 py-10'>
-            <h1 className='text-3xl'> MyTasky </h1>
+            <Link
+                href="/">
+                <h1 className='text-3xl'> MyTasky </h1>
+            </Link>
             <div className='flex gap-x-2 items-start'>
-                <Link href='/newTask'
-                    className={buttonVariants({ variant: 'secondary' })}
-                >
+                <Link
+                    href='/newTask'
+                    className={buttonVariants({ variant: 'secondary' })}>
                     Crear Tarea
                 </Link>
                 <ThemeToggle />
