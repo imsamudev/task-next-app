b73e904c09a8abdaab675f582a2fc798a5cc197a
SamuDev <samudev01@gmail.com>
2025-02-04 20:28:09 -0300

    homepage changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import TaskCard from "@/components/ui/taskCard"
 import prisma from "@/lib/prisma"
-import clsx from "clsx"
 
 async function HomePage() {
 
@@ -12,44 +9,7 @@ async function HomePage() {
   return (
     <div className="grid grid-cols-3 gap-4">
       {tasks.map((task) => (
-        <Card key={task.id}>
-          <CardHeader className="flex flex-row justify-between">
-            <CardTitle>
-              {task.name}
-            </CardTitle>
-            <Badge
-              className={
-                clsx({
-                  "bg-red-600": task.priority === "Urgente",
-                  "bg-orange-500": task.priority === "Alta",
-                  "bg-yellow-500": task.priority === "Media",
-                  "bg-green-500": task.priority === "Baja"
-                })
-              }>
-              {task.priority}
-            </Badge>
-          </CardHeader>
-          <hr className="my-1" />
-          <CardContent>
-            <div className="flex flex-col gap-y-4">
-              <p>{task.description}</p>
-              <hr className="my-1" />
-              <div className="flex flex-col">
-                <span className="text-slate-400">{new Date(task.createdAt).toLocaleDateString()}</span>
-                <span className="text-slate-500">{new Date(task.createdAt).toLocaleTimeString()}</span>
-              </div>
-            </div>
-          </CardContent>
-          <CardFooter className="flex gap-x-2 justify-end">
-            <Button variant={"destructive"}>
-              Borrar
-            </Button>
-            <Button>
-              Editar
-            </Button>
-          </CardFooter>
-        </Card>
-
+        <TaskCard task={task} key={task.id} />
       ))
       }
     </div >
